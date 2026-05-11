2710e28045728a2826c384a6ba0b582d42446261
Cassius <nils.lindstorm@gmail.com>
2026-05-11 14:53:23 +0200

    CMS doesn't function.. :(

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    
-    base: '/pages'
-    i18n: { // I18n enables multiple languages on the website.
+    // This enables multiple languages on the website.
+    i18n: { 
         locales: ["sv", "en"],  // This sets the available languages, sv = Swedish, en = English. You can in theory add more but it should be avoided.
         defaultLocale: "en", 
         routing: {
